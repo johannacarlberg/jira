d1021e72dbf9604fa4e57bb0f9ca7360fa539502
Johanna <jscarlberg@gmail.com>
2016-06-13 15:58:49 +0100

    loading hero-detail

--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -33,7 +33,7 @@ var HeroDetailComponent = (function () {
     HeroDetailComponent = __decorate([
         core_1.Component({
             selector: 'my-hero-detail',
-            template: "\n    <div *ngIf=\"hero\">\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"hero.name\" placeholder=\"name\"/>\n      </div>\n    </div>\n      <button (click)=\"goBack()\">Back</button>\n  "
+            templateUrl: 'app/hero-detail.component.html'
         }), 
         __metadata('design:paramtypes', [hero_service_1.HeroService, router_deprecated_1.RouteParams])
     ], HeroDetailComponent);
