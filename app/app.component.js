c4435e4299857a21320c9b0ec881084449e1b613
Johanna <jscarlberg@gmail.com>
2016-06-13 11:54:45 +0100

    making the app more modular

--- app/app.component.js
+++ app/app.component.js
@@ -9,12 +9,10 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var Hero = (function () {
-    function Hero() {
-    }
-    return Hero;
-}());
-exports.Hero = Hero;
+// export class Hero {
+//     id: number;
+//     name: string;
+// }
 var AppComponent = (function () {
     function AppComponent() {
         this.heroes = HEROES;
