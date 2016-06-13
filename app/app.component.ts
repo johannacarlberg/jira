18a9ee70bb837bc040b1ac991d93ee6bb03d67af
Johanna <jscarlberg@gmail.com>
2016-06-13 12:42:57 +0100

    get heroes slowly

--- app/app.component.ts
+++ app/app.component.ts
@@ -74,8 +74,9 @@ export class AppComponent implements OnInit {
     selectedHero: Hero;
     constructor(private heroService: HeroService) { }
     getHeroes() {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
     }
+    
     ngOnInit() {
         this.getHeroes();
     }
