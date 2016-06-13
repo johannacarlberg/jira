e28063b2bce5181a50acd8175565c53737c75c6e
Johanna <jscarlberg@gmail.com>
2016-06-13 12:35:59 +0100

    changing the gatheroes function to a promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -81,7 +81,7 @@ export class AppComponent implements OnInit {
     onSelect(hero: Hero) { this.selectedHero = hero; };
     constructor(private heroService: HeroService) { }
     getHeroes() {
-        this.heroes = this.heroService.getHeroes();
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
 
     ngOnInit() {
