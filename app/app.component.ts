de0baf0b12e485f2224eeebe3764fa1cad3c68b3
Johanna <jscarlberg@gmail.com>
2016-06-13 11:23:25 +0100

    setting up ngfor for iteration over heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,12 @@ export class Hero {
     selector: 'my-app',
     template:`
   <h1>{{title}}</h1>
+  <h2>My Heroes</h2>
+    <ul class="heroes">
+     <li *ngFor="let hero of heroes">
+    <!-- each hero goes here -->
+    </li>
+    </ul>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
   <div>
